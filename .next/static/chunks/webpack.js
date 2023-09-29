e8a5207efb0cacce0f1be88066b9955f1ad6fa3e
ChristianFajardo2022 <chfajard@gmail.com>
2023-09-29 16:27:56 -0500

    margenes cel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8447abdf12c9d89"; }
+/******/ 		__webpack_require__.h = function() { return "5696ea6af74ebdfd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
